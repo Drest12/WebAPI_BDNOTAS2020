11c0435279c00536fbc96a31aeb46f34daf72b19
Nestor Andres <107701223+Drest12@users.noreply.github.com>
2023-03-14 17:17:42 -0500

    Add files via upload

---
+++ WebAPI_BDNOTAS2020/Controllers/ConsultasController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WebAPI_BDNOTAS2020.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI_BDNOTAS2020.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultasController : ControllerBase
+    {
+        BDNOTAS2020okContext bd = new BDNOTAS2020okContext();
+
+        // public ActionResult<List<PA_ALUMNOS_CANTIDAD_PAGOS>> Lista_Alumnos_Pagos(string semestre1,
+        // string semestre2)
+        
+        // api/Consultas/2018-1/2020-2
+        [HttpGet("{semestre1}/{semestre2}")]
+        public List<PA_ALUMNOS_CANTIDAD_PAGOS> Lista_Alumnos_Pagos(string semestre1,
+                                                                   string semestre2)
+        {
+            var listado = bd.PA_ALUMNOS_CANTIDAD_PAGOS
+                            .FromSqlRaw<PA_ALUMNOS_CANTIDAD_PAGOS>(
+                                  "PA_ALUMNOS_CANTIDAD_PAGOS {0}, {1}", 
+                                  semestre1, semestre2)
+                            .ToList();
+            //
+            return listado;
+        }
+
+    }
+}
---
+++ WebAPI_BDNOTAS2020/Models/PA_ALUMNOS_CANTIDAD_PAGOS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_BDNOTAS2020.Models
+{
+    public class PA_ALUMNOS_CANTIDAD_PAGOS
+    {
+        [Key]
+        public string clave { get; set; }
+        public string codalumno { get; set; }
+        public string semestre { get; set; }
+        public string nomcompleto { get; set; }
+        public string nomesp { get; set; }
+        public int cantidad_pago { get; set; }
+        public int total { get; set; }
+    }
+}
